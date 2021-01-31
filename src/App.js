cc777ec6d5eacc7d83701678a53b14655a5097eb
Keshav Kaushal <32331754+keshavkaushal@users.noreply.github.com>
2021-01-31 12:12:31 +0000

    updates

--- src/App.js
+++ src/App.js
@@ -1,64 +1,49 @@
-import logo from './logo.svg';
 import original from './original.png';
 import superia from './superia.png';
-import vista from './vista.jpg';
-import vistaoriginal from './vistaoriginal.jpg';
+import dialog from './dialogtext.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <div className="top container">Photoshop Film Emulations</div>
-      <div className="intro container">
-        <h2>Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</h2>
-        <h3><a href="https://www.instagram.com/keshkaushal/">Instagram</a></h3>
-      </div>
-      <div className="film-wrapper0">
-      <div className="container">
-          <div className="film-title">Fujifilm Superia 200</div>
-          <div className="row">
-                <div className="img col-sm">
-                  <img src={original}></img>
-                </div>
-                <div className="img col-sm">
-                  <img src={superia}></img>
-                </div>
-          </div>
-                  <p>If you are going for stunning portraits outdoors or indoors with flash, you have to try Fuji Superia 200 35mm color negative film. Youll get clear photos with naturally beautiful colors and fine-grained smoothness!</p>
+      <div className="top txt-cen">
+        <div className="titlebar row">
+          <div className="col txt-l"><img className="dialog" src={dialog}></img></div>
+          <div className="col txt-r"><a href="https://www.instagram.com/shootdialog/"><i className="fa fa-instagram insta-title"></i></a></div>
         </div>
 
+        <div className="tagline">Recreate the organic look of analog films</div>
+        <div className="explore">Explore <i className="fa fa-chevron-down"></i></div>
       </div>
-      <div className="film-wrapper1">
-      <div className="container">
-          <div className="film-title">Agfa Vista 200</div>
-          <div className="row">
-                <div className="img col-sm">
-                  <img src={vistaoriginal}></img>
-                </div>
-                <div className="img col-sm">
-                  <img src={vista}></img>
-                </div>
-          </div>
-                  <p>Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
-        </div>
+      <div className="second container">
+        <div className="overview txt-cen">Overview</div>
+        <div className="overview-tagline txt-cen">Digital Tools Born from Film Emulsions</div>
+        <div className="overview-text txt-cen">Manually tweaked to recreate the look of clasic films, comes as .CUBE lookup tables which can be used in photo and video editing</div>
+      </div>
 
-      </div>
-      <div className="film-wrapper0">
-      <div className="container">
-          <div className="film-title">Fujifilm Velvia 50</div>
-          <div className="row">
-                <div className="img col-sm">
-                  <img src={original}></img>
-                </div>
-                <div className="img col-sm">
-                  <img src={superia}></img>
-                </div>
+      <div className="superia">
+        <div className="film-title txt-cen">Fujifilm Superia 200</div>
+        <div className="row">
+          <div className="img col">
           </div>
-                  <p>Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+          <div className="img col">
+          </div>
         </div>
-
+        <p>If you are going for stunning portraits outdoors or indoors with flash, you have to try Fuji Superia 200 35mm color negative film. Youll get clear photos with naturally beautiful colors and fine-grained smoothness!</p>
       </div>
-    <div className="footer">kjk</div>
+
+      <div className="superia">
+        <div className="film-title txt-cen">Fujifilm Superia 200</div>
+        <div className="row">
+          <div className="img col">
+          </div>
+          <div className="img col">
+          </div>
+        </div>
+        <p>If you are going for stunning portraits outdoors or indoors with flash, you have to try Fuji Superia 200 35mm color negative film. Youll get clear photos with naturally beautiful colors and fine-grained smoothness!</p>
+      </div>
+
+      <div className="footer">kjk</div>
     </div>
   );
 }

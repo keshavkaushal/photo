8ee9195f48a22f61e216247771108f88ba999405
Keshav Kaushal <32331754+keshavkaushal@users.noreply.github.com>
2021-01-31 16:59:25 +0000

    updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import original from './original.png';
-import superia from './superia.png';
+import superia from './superia.jpg';
 import dialog from './dialogtext.png';
 import './App.css';
 
@@ -11,39 +11,28 @@ function App() {
           <div className="col txt-l"><img className="dialog" src={dialog}></img></div>
           <div className="col txt-r"><a href="https://www.instagram.com/shootdialog/"><i className="fa fa-instagram insta-title"></i></a></div>
         </div>
-
         <div className="tagline">Recreate the organic look of analog films</div>
-        <div className="explore">Explore <i className="fa fa-chevron-down"></i></div>
+        <div className="explore"><i className="fa fa-chevron-down"></i></div>
       </div>
-      <div className="second container">
-        <div className="overview txt-cen">Overview</div>
-        <div className="overview-tagline txt-cen">Digital Tools Born from Film Emulsions</div>
-        <div className="overview-text txt-cen">Manually tweaked to recreate the look of clasic films, comes as .CUBE lookup tables which can be used in photo and video editing</div>
+      <div className="second">
+        <div className="container">
+          <div className="overview txt-cen">Overview</div>
+          <div className="overview-tagline txt-cen">Film rendering lookup tables</div>
+          <div className="overview-text txt-cen">Manually tweaked to recreate the look of clasic films, comes as .CUBE lookup tables which can be used in photo and video editing</div>
+        </div>
       </div>
 
-      <div className="superia">
-        <div className="film-title txt-cen">Fujifilm Superia 200</div>
-        <div className="row">
-          <div className="img col">
-          </div>
-          <div className="img col">
+      <div className="container">
+        <div className="card mb-3">
+          <img className="card-img-top" src={superia} alt="Card image cap"></img>
+          <div className="card-body">
+            <h5 className="card-title">Fujifilm Superia 200</h5>
+            <p className="card-text">Deep reds and softer highlights</p>
+            <p className="card-text"><small className="text-muted">£3.99</small></p>
           </div>
         </div>
-        <p>If you are going for stunning portraits outdoors or indoors with flash, you have to try Fuji Superia 200 35mm color negative film. Youll get clear photos with naturally beautiful colors and fine-grained smoothness!</p>
       </div>
-
-      <div className="superia">
-        <div className="film-title txt-cen">Fujifilm Superia 200</div>
-        <div className="row">
-          <div className="img col">
-          </div>
-          <div className="img col">
-          </div>
-        </div>
-        <p>If you are going for stunning portraits outdoors or indoors with flash, you have to try Fuji Superia 200 35mm color negative film. Youll get clear photos with naturally beautiful colors and fine-grained smoothness!</p>
-      </div>
-
-      <div className="footer">kjk</div>
+      <div className="footer">More coming soon. kjk</div>
     </div>
   );
 }
